75ace3dc2bf6702da0e54ba6e1748b52e13b624d
jeya-chandran-jcs <145828553+jeya-chandran-jcs@users.noreply.github.com>
2023-11-05 21:42:33 +0530

    Add files via upload

---
+++ day 16/script.js
@@ -0,0 +1,61 @@
+    
+            setTimeout(()=>{
+                let a=document.createElement("p")
+                a.innerText="1"
+                document.body.append(a)
+            
+                setTimeout(()=>{
+                let a=document.createElement("p")
+                a.innerText="2"
+                document.body.append(a)
+                    
+                setTimeout(()=>{
+                let a=document.createElement("p")
+                a.innerText="3"
+                document.body.append(a)
+                
+                setTimeout(()=>{
+                let a=document.createElement("p")
+                a.innerText="4"
+                document.body.append(a)
+                setTimeout(()=>{
+                    let a=document.createElement("p")
+                a.innerText="5"
+                document.body.append(a)
+                
+                setTimeout(()=>{
+                    let a=document.createElement("p")
+                a.innerText="6"
+                document.body.append(a)
+                setTimeout(()=>{
+                    let a=document.createElement("p")
+                a.innerText="7"
+                document.body.append(a)
+                setTimeout(()=>{
+                    let a=document.createElement("p")
+                a.innerText="8"
+                document.body.append(a)
+                setTimeout(()=>{
+                let a=document.createElement("p")
+                a.innerText="9"
+                document.body.append(a)
+                setTimeout(()=>{
+                let a=document.createElement("p")
+                a.innerText="10"
+                document.body.append(a)
+                setTimeout(()=>{
+                    let a=document.createElement("p")
+                a.innerText="happy independence day"
+                document.body.append(a)
+            },1000)
+        },1000)
+        },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    
