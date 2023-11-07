4a6e739a0aa41e910f1c50c990571c8569ce5c30
jeya-chandran-jcs <145828553+jeya-chandran-jcs@users.noreply.github.com>
2023-11-08 00:36:19 +0530

    script.js

--- day 20/screenshot/script.js
+++ day 20/screenshot/script.js
@@ -3,18 +3,17 @@ function captureScreenshot() {
     const apiKey = "7def307631ed4d599b2162a4ce907407";
     const screenshotContainer = document.getElementById("screenshot-container");
 
-    // Construct the screenshot URL with the given URL and API key
+    //screenshot URL and given URL and API key
     const screenshotUrl = `https://screenshot.abstractapi.com/v1/?api_key=${apiKey}&url=${url}`;
 
-    // Clear any previous screenshots
     screenshotContainer.innerHTML = "";
 
-    // Create an image element to display the screenshot
+    // Create an image 
     const screenshotImage = document.createElement("img");
     screenshotImage.src = screenshotUrl;
     screenshotImage.alt = "Web Page Screenshot";
     screenshotImage.style.maxWidth = "100%";
 
-    // Append the screenshot image to the container
+    // Append the screenshot 
     screenshotContainer.appendChild(screenshotImage);
 }
